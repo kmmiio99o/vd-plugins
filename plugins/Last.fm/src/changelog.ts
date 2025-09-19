1ca490919a501cdeaa67a0d531ab8c7b70da8165
kmmiio99o <164557248+kmmiio99o@users.noreply.github.com>
2025-09-19 22:20:01 +0200

    update date

--- plugins/Last.fm/src/changelog.ts
+++ plugins/Last.fm/src/changelog.ts
@@ -7,7 +7,7 @@ interface ChangelogEntry {
 export const changelog: ChangelogEntry[] = [
   {
     version: "2.3.0",
-    date: "2025-09-13",
+    date: "2025-09-19",
     changes: [
       "1. I TRIED SO HARD 2. remove backend error 3. factor settings tab 4. factor V2 5. factor V3 6. factor V4 7. factor V5 8. factor V6 9. Full settings refactor :3",
     ],
