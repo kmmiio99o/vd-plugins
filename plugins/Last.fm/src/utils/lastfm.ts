38d9297fe4112fbc3e2446160714f8b601bfe9bb
kmmiio99o <szymonladziak00@gmail.com>
2025-10-01 19:05:50 +0200

    small change

--- plugins/Last.fm/src/utils/lastfm.ts
+++ plugins/Last.fm/src/utils/lastfm.ts
@@ -71,7 +71,6 @@ class LastFMClient {
 
     const errorMessages: { [key: number]: string } = {
       2: "Invalid API key",
-      6: "User not found",
       11: "Service temporarily unavailable",
       16: "Service temporarily unavailable",
       29: "Rate limit exceeded",
